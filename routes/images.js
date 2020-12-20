8d5900e2f6d4102957bc861ff44a9eb6d6fbf9b3
ghassenWalha <walha.ghassen@insat.u-carthage.tn>
2020-12-20 09:04:37 +0100

    adding image uplaod fucntionality

---
+++ routes/images.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { cloudinary } = require('../utils/cloudinary');
+
+
+router.post('/', async (req, res) => {
+    try {
+        const fileStr = req.body.data;
+        console.log(req.body)
+        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset: 'dev_setups',
+        });
+        console.log(uploadResponse);
+        res.json({ url: uploadResponse.secure_url });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+});
+
+
+module.exports = router;
