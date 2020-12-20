2309248423c4d43272cbcce86e66d29bb6188e3d
ghassenWalha <67965351+ghassenWalha@users.noreply.github.com>
2020-12-20 20:33:10 +0100

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -42,7 +42,7 @@ router.get('/product/:id', async (req, res) => {
 })
 
 // creating a new product
-    router.post('/', /*[auth, admin],*/ async (req, res) => {
+    router.post('/', [auth, admin], async (req, res) => {
     const {name, description, moreInfo, price, category, imgsUrl:imgUrls,color} = req.body;
         console.log(req.body);
     const product = new Product({name, description, moreInfo, price, category, imgUrls,color});
